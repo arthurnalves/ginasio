70bf92fb47ee9e1dfcf47d21c7c28b21605fee43
arthurnalves <arthurnalves@gmail.com>
2021-10-26 10:19:40 +0000

    logic: add data model

--- django_in_a_box/api/logic/models.py
+++ django_in_a_box/api/logic/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 class Agent(models.Model):
 	genome = models.TextField(null=True)
 
-
+	
 class Environment(models.Model):
 	name = models.CharField(max_length=100, null=True)
+
+
+class Data(models.Model):
+	inputs = ArrayField(models.FloatField())
+	ouputs = ArrayField(models.FloatField())
+	environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
