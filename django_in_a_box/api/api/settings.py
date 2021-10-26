b5766d9e6c3d0a94ebf4a4173fb1271371b33166
arthurnalves <arthurnalves@gmail.com>
2021-10-26 09:03:15 +0000

    docker: rename

--- django_in_a_box/api/api/settings.py
+++ django_in_a_box/api/api/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': 5432,
     }
 }
